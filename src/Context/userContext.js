fc958d0e24b5a8807e914b2805240b0e803f81ce
uzair-anwar <uzair.anwar@devsinc.com>
2022-08-09 22:43:19 +0500

    Changed state of user from empty to null

--- src/Context/userContext.js
+++ src/Context/userContext.js
@@ -2,7 +2,7 @@ import React, { createContext, useState } from "react";
 export const UserContext = createContext();
 export function UserProvider({ children }) {
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
   const [token, setToken] = useState("");
   return (
     <UserContext.Provider value={{ user, setUser, token, setToken }}>
