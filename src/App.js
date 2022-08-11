d501cc5983017b8fe89ce6eea60e6239f163dba1
uzair-anwar <107038841+uzair-anwar@users.noreply.github.com>
2022-08-11 17:35:43 +0500

    Added accounts module containing signup and login features (#1)
    * Completed users module, Called backend APIs
    
    * Main component changed to page
    
    * Changed state of user from empty to null
    
    * Solved env variables accessing issue
    
    * Resolved token issue and apply toaster on signup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { UserProvider } from "./Context/userContext";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Main from "./Pages/Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <UserProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/posts" element={<Main />} />
+        </Routes>
+      </UserProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Services/accounts.js
@@ -0,0 +1,32 @@
+const axios = require("axios").default;
+
+const signup = async (user) => {
+  try {
+    const response = await axios.post(
+      `${process.env.REACT_APP_SERVER_API}/auth/signup`,
+      {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+};
+const login = async (user) => {
+  try {
+    const response = await axios.post(
+      `${process.env.REACT_APP_SERVER_API}/auth/login`,
+      {
+        email: user.email,
+        password: user.password,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+};
+module.exports = { signup, login };
